04959f025b3c434249ad4a012660f8df949bc723
RayanElChakik <rayan.elchakik@gmail.com>
2021-12-22 18:50:52 +0000

    remove

--- tasks.js
+++ tasks.js
@@ -45,6 +45,8 @@ function onDataReceived(text) {
     li();
   }else if(text.trim().split(" ")[0] === 'add'){
     add(text)
+  }else if(text.trim().split(" ")[0] === 'remove'){
+    remove(text.trim().split(" ")[1]);
   }
   else{
     unknownCommand(text);
@@ -103,6 +105,21 @@ function unknownCommand(c){
       console.log('Error! Yoou Should add a task to the list.')
     }
 }
+/**
+ * Removing items from the list
+ *
+ * @returns {void}
+ */
+ function remove(taskR){
+  var index = Number(taskR);
+  if(isNaN(index)){
+   tasksList.splice(tasksList.length-1,1);
+  }else {
+   tasksList.splice(index-1,1);
+ }
+ console.log("Your task have been deleted. Check the new upadted list if u want!")
+}
+
 
 /**
  * Exits the application
